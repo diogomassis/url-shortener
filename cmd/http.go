c31afa8c287459b7838166e9b976cf7bb5f9dc72
Diogo Martins de Assis <diogomartinsdassis@gmail.com>
2025-12-14 14:18:06 -0300

    feat: wire up hexagonal architecture in main application

--- cmd/http.go
+++ cmd/http.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/ansrivas/fiberprometheus/v2"
+	"github.com/diogomassis/url-shortener/internal/adapters/handler/http"
+	"github.com/diogomassis/url-shortener/internal/adapters/repository/memory"
+	"github.com/diogomassis/url-shortener/internal/core/services"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,6 +14,18 @@ func main() {
 	prometheus := fiberprometheus.New("url-shortener")
 	prometheus.RegisterAt(app, "/metrics")
 	app.Use(prometheus.Middleware)
+
+	// Adapters
+	repo := memory.NewMemoryRepository()
+
+	// Services
+	service := services.NewURLService(repo)
+
+	// Handlers
+	handler := http.NewHTTPHandler(service)
+
+	app.Post("/api/v1", handler.Shorten)
+	app.Get("/:shortCode", handler.Redirect)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Welcome to URL Shortener!")
--- go.mod
+++ go.mod
@@ -7,13 +7,13 @@ toolchain go1.24.11
 require (
 	github.com/ansrivas/fiberprometheus/v2 v2.14.0
 	github.com/gofiber/fiber/v2 v2.52.10
+	github.com/google/uuid v1.6.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
